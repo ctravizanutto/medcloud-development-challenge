51029deb12903095c8c72dc1e9126e6c487787b3
ctravizanutto <ctravizanutto@gmail.com>
2024-09-11 15:04:39 -0300

    Revert "rename backend create frontend folder"
    This reverts commit c0810dbe5eb31fe6f9763ff501d03faa43c3e9c8.

---
+++ back/src/schemas/address.schema.ts
@@ -0,0 +1,29 @@
+import Joi from "joi";
+import {getStates, isValidCEP} from "@brazilian-utils/brazilian-utils";
+import {Address} from "@prisma/client";
+
+export type AddressParams = Omit<Address, "id" | "createdAt" | "updatedAt">
+
+const cepValidationSchema = Joi.string().length(9).custom(JoiCepValidation).required();
+export const addressSchema = Joi.object({
+    cep: cepValidationSchema,
+    street: Joi.string().required(),
+    city: Joi.string().required(),
+    number: Joi.string().required(),
+    state: Joi.string()
+        .length(2)
+        .valid(...getStates().map((s) => s.code))
+        .required(),
+    neighborhood: Joi.string().required(),
+    addressDetail: Joi.string().allow(null, ""),
+})
+
+function JoiCepValidation(value: string, helpers: Joi.CustomHelpers<string>) {
+    if (!value) return value;
+
+    if (!isValidCEP(value)) {
+        return helpers.error("any.invalid");
+    }
+
+    return value;
+}
---
+++ back/src/services/patient.service.ts
@@ -0,0 +1,82 @@
+import {Patient} from "@prisma/client";
+import patientRepository from "../repositories/patient.repository.js";
+import {duplicatedEmailError} from "../errors/duplicate-email.error.js";
+import {PatientParams} from "../schemas/patient.schema.js";
+import addressRepository from "../repositories/address.repository.js";
+import {patientNotExistsError} from "../errors/patient-not-exists.error.js";
+
+async function createPatient(params: PatientParams): Promise<Patient> {
+    await validateUniqueEmail(params.email)
+
+    const {address, ...patientData} = params;
+
+    return await patientRepository.create({
+        name: patientData.name,
+        email: patientData.email,
+        birthday: new Date(patientData.birthday),
+        address: {
+            create: address,
+        },
+    })
+}
+
+async function listAllPatients(pageNumber: number, limit: number): Promise<Patient[]> {
+    return await patientRepository.listAll(pageNumber, limit);
+}
+
+async function getPatientCount(): Promise<number> {
+    return await patientRepository.count()
+}
+
+async function detailPatient(id: string): Promise<Patient> {
+    await validateExists(id)
+
+    return await patientRepository.findById(id)
+}
+
+async function update(id: string, params: PatientParams): Promise<Patient> {
+    await validateExists(id)
+
+    const {address, ...patientData} = params;
+
+    if (address) {
+        await addressRepository.update(id, address)
+    }
+    if (params.email) {
+        await validateUniqueEmail(params.email)
+        await patientRepository.update(id, patientData)
+    }
+
+    return await patientRepository.findById(id)
+}
+
+async function disable(id: string) {
+    await validateExists(id)
+
+    return await patientRepository.disable(id)
+}
+
+async function validateUniqueEmail(email: string) {
+    const patient = await patientRepository.findByEmail(email)
+    if (patient) {
+        throw duplicatedEmailError()
+    }
+}
+
+async function validateExists(id: string) {
+    const patient = await patientRepository.findById(id)
+    if (!patient) {
+        throw patientNotExistsError()
+    }
+}
+
+const patientService = {
+    createPatient,          // C
+    listAllPatients,        // R
+    getPatientCount,
+    detailPatient,
+    update,                 // U
+    disable                 // D
+}
+
+export default patientService
