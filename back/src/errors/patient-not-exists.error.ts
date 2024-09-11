c0810dbe5eb31fe6f9763ff501d03faa43c3e9c8
ctravizanutto <ctravizanutto@gmail.com>
2024-09-11 15:04:07 -0300

    rename backend create frontend folder

--- back/src/errors/patient-not-exists.error.ts
+++
@@ -1,8 +0,0 @@
-import {ApplicationError} from "./protocol.js";
-
-export function patientNotExistsError(): ApplicationError {
-    return {
-        name: "PatientNotExistsError",
-        message: "There is not a patient with given id",
-    };
-}
--- back/src/server.ts
+++
@@ -1,9 +0,0 @@
-import app, {init} from "./app.js";
-
-const port = 8080
-
-init().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is listening on port ${port}.`)
-    })
-})
