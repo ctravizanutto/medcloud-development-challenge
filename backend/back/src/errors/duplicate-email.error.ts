58a369352cf64e05a71e3ed5c4d2f7753fa1cc65
ctravizanutto <ctravizanutto@gmail.com>
2024-09-13 18:08:53 -0300

    backend delete unknown back folder

--- backend/back/src/errors/duplicate-email.error.ts
+++
@@ -1,8 +0,0 @@
-import {ApplicationError} from "./protocol.js";
-
-export function duplicatedEmailError(): ApplicationError {
-    return {
-        name: "DuplicatedEmailError",
-        message: "There is already an user with given email",
-    };
-}
